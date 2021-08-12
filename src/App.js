efae75ae95d41c49c7ec56decb022d695b9825d9
unknown <saransh.tyagi@soprasteria.com>
2021-08-12 18:11:46 +0530

    counter with redux actions(used use selector)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment } from './Actions/index';
+import { decrement } from './Actions/index';
+
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
   return (
-    <h1>Ready to work</h1>
+    <>
+      <div className="App">
+        <h1>Counter {counter}</h1>
+        <button className='item' onClick={() => dispatch(increment())}>+</button>
+        <button className='item' onClick={() => dispatch(decrement())}>-</button>
+        {isLogged ? <h3>For authorized personal only</h3> : " "}
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,34 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
+import allReducers from "./Reducers/index";
+import {Provider} from "react-redux";
 
-
-const increment = () => {
-  return { type: "INCREMENT" }
-}
-const decrement = () => {
-  return { type: "DECREMENT" }
-}
-//reducer will tell us what is pupposed to happen when we perform an action
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT": return state + 1
-    case "DECREMENT": return state - 1
-  }
-}
-let store = createStore(counter);
-store.subscribe(()=> console.log(store.getState()));
-//dispatcher
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(increment());
+const store=createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
